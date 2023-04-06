485e5bd5c383eb7a66e468267a0fc7816a3b8cb0
konstantin.8k <khomerikikote11@gmail.com>
2023-04-06 21:46:08 +0400

    konstansin-khomeriki-hw-1

---
+++ app/src/main/java/com/example/konstantinkhomerikihw1/SecondPage.kt
@@ -0,0 +1,11 @@
+package com.example.konstantinkhomerikihw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_page)
+    }
+}
